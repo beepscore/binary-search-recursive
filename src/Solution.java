2c5f9814b50947b7df74f060503ae77c8efadd75
beepscore <support@beepscore.com>
2017-04-10 11:20:15 -0700

    In Solution use array of primitive type int instead of ArrayList of Integer

--- src/Solution.java
+++ src/Solution.java
@@ -2,13 +2,20 @@
  * Created by stevebaker on 4/10/17.
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
+// https:// www.youtube.com/watch?v=P3YID7liBug&index=26&list=PLX6IKgS15Ue02WDPRCmYKuZicQHit9kFt
 
 public class Solution {
 
-    List<Element> sorted_list = Arrays.asList(2, 4, 5, 8, 14, 19, 20, 25, 27, 31, 44, 53, 59, 88, 98);
+    public static void main(String[] args) {
 
+        int[] sorted_array = new int[] {2, 4, 5, 8, 14, 19, 20, 25, 27, 31, 44, 53, 59, 88, 98};
 
+        Boolean result = Solution.contains(sorted_array, 3);
+        System.out.println(result);
+    }
+
+    public static Boolean contains(int[] sorted_array, int number) {
+        return Boolean.TRUE;
+    }
 
 }
