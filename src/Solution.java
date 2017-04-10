d7f67e69f5014a244b96cbc9a958f2a5212035fa
beepscore <support@beepscore.com>
2017-04-10 13:31:26 -0700

    Print array

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by stevebaker on 4/10/17.
  */
@@ -10,6 +12,7 @@ public class Solution {
 
         int[] sorted_array = new int[] {2, 4, 5, 8, 14, 19, 20, 25, 27, 31, 44, 53, 59, 88, 98};
 
+        System.out.println(Arrays.toString(sorted_array));
         Boolean result = Solution.contains(sorted_array, 3);
         System.out.println(result);
     }
