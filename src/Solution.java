e0862b96af60574bba5a160d403112b6df99330d
beepscore <support@beepscore.com>
2017-04-10 13:58:09 -0700

    Implement contains.

--- src/Solution.java
+++ src/Solution.java
@@ -13,12 +13,50 @@ public class Solution {
         int[] sorted_array = new int[] {2, 4, 5, 8, 14, 19, 20, 25, 27, 31, 44, 53, 59, 88, 98};
 
         System.out.println(Arrays.toString(sorted_array));
-        Boolean result = Solution.contains(sorted_array, 3);
+        // test extremes
+        Boolean result = Solution.contains(sorted_array, 2, 0, sorted_array.length - 1);
+        System.out.println(result);
+        result = Solution.contains(sorted_array, 98, 0, sorted_array.length - 1);
+        System.out.println(result);
+
+        result = Solution.contains(sorted_array, 2001, 0, sorted_array.length - 1);
+        System.out.println(result);
+
+        result = Solution.contains(sorted_array, 3, 0, sorted_array.length - 1);
+        System.out.println(result);
+
+        result = Solution.contains(sorted_array, 5, 0, sorted_array.length - 1);
+        System.out.println(result);
+
+        result = Solution.contains(sorted_array, 53, 0, sorted_array.length - 1);
         System.out.println(result);
     }
 
-    public static Boolean contains(int[] sorted_array, int number) {
-        return Boolean.TRUE;
+    public static Boolean contains(int[] sorted_array, int number, int index_min, int index_max) {
+
+        int index_mid = index_min + ((index_max - index_min)/2);
+        System.out.println(String.format("number %d, index_min %d,  index_max %d, index_mid %d", number, index_min, index_max, index_mid));
+
+        while (index_min <= index_max) {
+
+            if (sorted_array[index_mid] == number) {
+                return Boolean.TRUE;
+            }
+
+            if (sorted_array[index_mid] < number) {
+                // already checked index_mid
+                index_min = index_mid + 1;
+                return Solution.contains(sorted_array, number, index_min, index_max);
+            }
+
+            if (sorted_array[index_mid] > number) {
+                // already checked index_mid
+                index_max = index_mid - 1;
+                return Solution.contains(sorted_array, number, index_min, index_max);
+            }
+        }
+
+        return Boolean.FALSE;
     }
 
 }
