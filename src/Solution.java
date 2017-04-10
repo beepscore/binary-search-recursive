55a97361397ca0ac3d0c85ccbf97e0c4ef92cd82
beepscore <support@beepscore.com>
2017-04-10 10:39:36 -0700

    Change sdk to java 1.8. Add class Solution. hackerrank requires class name Solution, no package names
    path to SDK
    echo $(/usr/libexec/java_home)
    /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home
    http://stackoverflow.com/questions/16765726/how-to-set-intellij-idea-project-sdk#16765841

---
+++ src/Solution.java
@@ -0,0 +1,14 @@
+/**
+ * Created by stevebaker on 4/10/17.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+
+    List<Element> sorted_list = Arrays.asList(2, 4, 5, 8, 14, 19, 20, 25, 27, 31, 44, 53, 59, 88, 98);
+
+
+
+}
